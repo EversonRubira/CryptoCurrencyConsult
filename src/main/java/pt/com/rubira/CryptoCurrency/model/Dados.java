f8810e2b0597eeef284484c13c4c2d134768d424
EversonRubira <eversonrubira@gmail.com>
2023-10-27 21:20:41 +0100

    Finalizando a App

---
+++ src/main/java/pt/com/rubira/CryptoCurrency/model/Dados.java
@@ -0,0 +1,22 @@
+package pt.com.rubira.CryptoCurrency.model;
+
+public class Dados {
+
+    private String coins;
+
+    public Dados() {
+
+    }
+
+    public Dados(String coins) {
+        this.coins = coins;
+    }
+
+    public String getCoins() {
+        return coins;
+    }
+
+    public void setCoins(String coins) {
+        this.coins = coins;
+    }
+}
--- src/main/java/pt/com/rubira/CryptoCurrency/principal/Principal.java
+++ src/main/java/pt/com/rubira/CryptoCurrency/principal/Principal.java
@@ -1,6 +1,9 @@
 package pt.com.rubira.CryptoCurrency.principal;
 
+import pt.com.rubira.CryptoCurrency.model.Dados;
 import pt.com.rubira.CryptoCurrency.service.ConsumoApi;
+import pt.com.rubira.CryptoCurrency.service.ConverteDados;
+
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,49 +11,76 @@ public class Principal {
 
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
 
     private final String URL_BASE = "https://www.mercadobitcoin.net/api/";
 
     public void ExibeMenu() throws IOException {
+        String json = null;
         boolean continuar = true;
 
         while (continuar) {
             System.out.println(" *** OPÇÕES CRYPTO ***");
-            System.out.println("1. Buscar informações de uma criptomoeda");
-            System.out.println("2. Sair");
+            System.out.println("1. Lista de Cryptomoedas");
+            System.out.println("2. Digite 2 e em seguida a criptomoeda, para informações detalhadas");
+            System.out.println("3. Sair");
 
             int escolha = leitura.nextInt();
 
             switch (escolha) {
                 case 1:
-                    System.out.print("Digite a sigla da criptomoeda (por exemplo, BTC): ");
-                    leitura.nextLine(); // Limpa o buffer
-                    String sigla = leitura.nextLine();
-                    String urlDaCriptomoeda = URL_BASE + sigla + "/ticker";
+                    String urlCriptoMoedas = URL_BASE + "coins";
                     try {
-                        String json = consumo.obterDados(urlDaCriptomoeda);
+                        json = consumo.obterDados(urlCriptoMoedas);
                         if (json != null) {
-                            System.out.println("Informações da criptomoeda " + sigla + ":");
+                            System.out.print("Listando moedas Disponiveis: ");
+                            var coins = conversor.obterLista(json, Dados.class);
+                            coins.forEach(coin -> System.out.println(coin.getCoins()));
+
+                        } else {
+                            System.out.println("Erro ao buscar moedas disponiveis.");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("Erro ao buscar moedas disponiveis.");
+                    }
+                    break;
+
+
+                case 2:
+                    leitura.nextLine(); // Limpa o buffer
+                    String coins = leitura.nextLine();
+                    String urlDaCriptomoeda = URL_BASE + coins + "/ticker";
+                    try {
+                        json = consumo.obterDados(urlDaCriptomoeda);
+                        if (json != null) {
+                            System.out.println("Informações da criptomoeda " + coins + ":");
                             System.out.println(json);
                         } else {
-                            System.out.println("Erro ao buscar informações da criptomoeda " + sigla);
+                            System.out.println("Erro ao buscar informações da criptomoeda " + coins);
                         }
-                    }catch (IOException e) {
+                    } catch (IOException e) {
                         e.printStackTrace();
-                        System.out.println("Erro ao buscar informações da criptomoeda " + sigla ": está sigla nao consta em nossos registros.");
+                        System.out.println("Erro ao buscar informações da criptomoeda " + coins + ": está sigla nao consta em nossos registros.");
                     }
-
                     break;
-                case 2:
+
+
+                case 3:
                     continuar = false;
                     System.out.println("Saindo do programa.");
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
+
+
+
             }
         }
-    }
 
 
+
     }
+}
 
